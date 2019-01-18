fec3d6a716510ac798245d68efe19b27857f9c5d
Julian Lundquist <julian.j.lundquist@gmail.com>
2019-01-18 14:28:10 -0600

    /posts refactored to /products

--- src/main/java/com/example/springblog/controllers/ProductController.java
+++ src/main/java/com/example/springblog/controllers/ProductController.java
@@ -16,51 +16,51 @@ public class ProductController {
         this.productService = ps;
     }
 
-    @GetMapping("/posts")
+    @GetMapping("/products")
     public String show(Model model) {
-        model.addAttribute("posts", productService.getAll());
-        return "posts/index";
+        model.addAttribute("products", productService.getAll());
+        return "products/index";
     }
 
-    @GetMapping("/posts/{id}")
+    @GetMapping("/products/{id}")
     public String postid(@PathVariable int id, Model model) {
         model.addAttribute("post", productService.findOne(id));
         model.addAttribute("id", id);
-        return "posts/show";
+        return "products/show";
     }
 
-    @GetMapping("/posts/create")
+    @GetMapping("/products/create")
     public String showCreateForm(Model model) {
         model.addAttribute("post", new Product());
-        return "posts/create";
+        return "products/create";
     }
 
-    @PostMapping("/posts/create")
+    @PostMapping("/products/create")
     public String create(@ModelAttribute Product product) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         product.setUser(user);
         productService.create(product);
-        return "redirect:/posts";
+        return "redirect:/products";
         // save the ad...
     }
 
-    @GetMapping("/posts/{id}/edit")
+    @GetMapping("/products/{id}/edit")
     public String edit(@PathVariable int id, Model model) {
         model.addAttribute("post", productService.findOne(id));
-        return "posts/edit";
+        return "products/edit";
     }
 
-    @GetMapping("/posts/{id}/delete")
+    @GetMapping("/products/{id}/delete")
     public String delete(@PathVariable int id, Model model) {
         model.addAttribute("post", productService.findOne(id));
         productService.delete(productService.findOne(id));
-        return "redirect:/posts";
+        return "redirect:/products";
     }
 
-    @PostMapping("/posts/{id}/edit")
+    @PostMapping("/products/{id}/edit")
     public String edit(@ModelAttribute Product product) {
         productService.edit(product);
-        return "redirect:/posts";
+        return "redirect:/products";
         // save the ad...
     }
 }
