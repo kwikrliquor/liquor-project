456a9087f66ab625bb96bb173e6507547dbcb3f2
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-01-22 11:40:08 -0600

    created id variable in UserReview model

--- src/main/java/com/example/springblog/models/UserReview.java
+++ src/main/java/com/example/springblog/models/UserReview.java
@@ -1,4 +1,13 @@
 package com.example.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_reviews")
 public class UserReview {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+
 }
