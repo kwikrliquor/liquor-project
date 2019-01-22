d428d300074794031218e2a19a6efc7f4b999848
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-01-21 18:44:38 -0600

    added annotations, properties, constructors and setters and getters in Product model to generate proper products db table

--- src/main/java/com/example/springblog/models/Product.java
+++ src/main/java/com/example/springblog/models/Product.java
@@ -1,29 +1,70 @@
 package com.example.springblog.models;
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@Table(name = "products")
 public class Product {
+
+
     @Id @GeneratedValue
     private int id;
 
-    @Column(nullable = false, length = 100)
-    private String title;
+    @Column(nullable = false)
+    private String name;
 
     @Column(nullable = false)
-    private String body;
+    private int stock;
+
+    @Column(nullable = false)
+    private double cost;
+
+    @Column(name = "img_url", nullable = false)
+    private String imgUrl;
 
     @ManyToOne
-    @JoinColumn (name = "user_id")
+    @JoinColumn(name = "cat_id")
+    private Category category = new Category();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "products_orders",
+            joinColumns = {@JoinColumn(name = "product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "order_id")}
+    )
+    private List<Order> orders;
+
+
+//============================================== Blog Code START
+//    @Column(nullable = false, length = 100)
+    private String title;
+
+//    @Column(nullable = false)
+    private String body;
+
+//    @ManyToOne
+//    @JoinColumn (name = "user_id")
     private User user;
+//================================================================== Blog Code END
 
     public Product() {
     }
 
+    public Product(String name, int stock, double cost, String imgUrl, Category category) {
+        this.name = name;
+        this.stock = stock;
+        this.cost = cost;
+        this.imgUrl = imgUrl;
+        this.category = category;
+    }
+
+//============================================== Blog Code START
     public Product(String title, String body, User user) {
         this.title = title;
         this.body = body;
         this.user = user;
     }
+//================================================================== Blog Code END
 
     public int getId() {
         return id;
@@ -33,6 +74,49 @@ public class Product {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+
+
+//============================================== Blog Code START
     public String getTitle() {
         return title;
     }
@@ -56,4 +140,5 @@ public class Product {
     public void setUser(User user) {
         this.user = user;
     }
+//================================================================== Blog Code END
 }
