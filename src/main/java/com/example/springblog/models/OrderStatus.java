28516108274c9fab8c5cef890189646cf4391ebd
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-01-22 10:58:15 -0600

    added OrderStatus model

---
+++ src/main/java/com/example/springblog/models/OrderStatus.java
@@ -0,0 +1,37 @@
+package com.example.springblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_status")
+public class OrderStatus {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(nullable = false, length = 100)
+	private String status;
+
+	public OrderStatus() {
+	}
+
+	public OrderStatus(String status) {
+		this.status = status;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
