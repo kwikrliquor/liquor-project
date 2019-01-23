17659100a0b2b9d8a6a4148b50167d489d06bdd1
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-01-23 16:52:55 -0600

    add functionality to edit user profile, working on retaining existing password upon edit.

--- src/test/drop_kl_db_tables.sql
+++ src/test/drop_kl_db_tables.sql
@@ -9,3 +9,11 @@ DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 
 
+
+UPDATE users
+SET first_name = 'John', last_name = 'Smith', address1 = "123 Bakers Ln",
+    city = 'San Antonio', state = 'TX', postal_code = 78254,  phone_number = 2106574596
+    WHERE id = 1;
+
+
+SELECT * FROM users;
