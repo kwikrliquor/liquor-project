e53e86a6affc59e28d4e3aebc4cb3c9589c180db
Julian Lundquist <julian.j.lundquist@gmail.com>
2019-01-24 10:45:26 -0600

    all products added to the home page

--- src/main/java/com/example/springblog/controllers/HomeController.java
+++ src/main/java/com/example/springblog/controllers/HomeController.java
@@ -1,14 +1,23 @@
 package com.example.springblog.controllers;
 
+import com.example.springblog.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+  private ProductService productService;
+
+  public HomeController(ProductService productService) {
+    this.productService = productService;
+  }
+
   @GetMapping("/")
-  public String home() {
+  public String home(Model model) {
+    model.addAttribute("products", productService.getAll());
     return "home";
   }
 
