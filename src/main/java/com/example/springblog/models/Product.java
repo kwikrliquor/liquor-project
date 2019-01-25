88b03da31872dd19116abf71939b4136a3b3b96b
Julian Lundquist <julian.j.lundquist@gmail.com>
2019-01-25 16:31:04 -0600

    cart

--- src/main/java/com/example/springblog/models/Product.java
+++ src/main/java/com/example/springblog/models/Product.java
@@ -13,66 +13,66 @@ import java.math.BigDecimal;
 @Table(name = "products")
 public class Product {
 
-  @Id @GeneratedValue
-  private long id;
+    @Id @GeneratedValue
+    private long id;
 
-  @Column(nullable = true)
-  private int stock;
+    @Column(nullable = true)
+    private int stock;
 
-  @Column(nullable = true)
-  private int quantity;
+    @Column(nullable = true)
+    private int quantity;
 
-  @Column(nullable = true)
-  @DecimalMin(value = "0.00", message = "*Price has to be non negative number")
-  private BigDecimal price;
+    @Column(nullable = true)
+    @DecimalMin(value = "0.00", message = "*Price has to be non negative number")
+    private BigDecimal price;
 
-  @Column(name = "img_url", nullable = true)
-  private String imgUrl;
+    @Column(name = "img_url", nullable = true)
+    private String imgUrl;
 
-  @ManyToOne
-  @JoinColumn(name = "cat_id")
-  private Category category; // Not sure if this is correct
+    @ManyToOne
+    @JoinColumn(name = "cat_id")
+    private Category category; // Not sure if this is correct
 
-  @Column(nullable = false, length = 1000)
-  private String name;
+    @Column(nullable = false, length = 1000)
+    private String name;
 
-  @Column(nullable = false, length = 10000)
-  private String type;
+    @Column(nullable = false, length = 10000)
+    private String type;
 
-  @Column(nullable = false, length = 10000)
-  private String description;
+    @Column(nullable = false, length = 10000)
+    private String description;
 
-  @ManyToOne
-  @JoinColumn (name = "user_id")
-  private User user;
+    @ManyToOne
+    @JoinColumn (name = "user_id")
+    private User user;
 
-  public Product() {
-  }
+    public Product() {
+    }
 
-  public Product(String name, String type, String description, User user) {
+    public Product(String name, String type, String description, User user) {
     this.name = name;
     this.type = type;
     this.description = description;
     this.user = user;
-  }
+    }
 
-  public Product(String name, String type, String description, Category category) {
+    public Product(String name, String type, String description, Category category) {
     this.name = name;
     this.type = type;
     this.description = description;
     this.category = category;
-  }
+    }
 
-  public Product(String name, String type, String description, BigDecimal price, Category category, User user) {
+    public Product(String name, String type, String description, BigDecimal price, Category category, User user) {
     this.name = name;
     this.type = type;
     this.description = description;
     this.price = price;
     this.category = category;
     this.user = user;
-  }
+    }
 
-  public Product(int stock, int quantity, BigDecimal price, String imgUrl, Category category, String name, String type, String description, User user) {
+    public Product(int stock, int quantity, BigDecimal price, String imgUrl, Category category, String name, String type, String description, User user) {
     this.stock = stock;
     this.quantity = quantity;
     this.price = price;
@@ -82,85 +82,100 @@ public class Product {
     this.type = type;
     this.description = description;
     this.user = user;
-  }
+    }
 
-  public long getId() {
+    public long getId() {
     return id;
-  }
+    }
 
-  public void setId(long id) {
+    public void setId(long id) {
     this.id = id;
-  }
+    }
 
-  public int getStock() {
+    public int getStock() {
     return stock;
-  }
+    }
 
-  public void setStock(int stock) {
+    public void setStock(int stock) {
     this.stock = stock;
-  }
+    }
 
-  public int getQuantity() {
+    public int getQuantity() {
     return quantity;
-  }
+    }
 
-  public void setQuantity(int quantity) {
+    public void setQuantity(int quantity) {
     this.quantity = quantity;
-  }
+    }
 
-  public BigDecimal getPrice() {
+    public BigDecimal getPrice() {
     return price;
-  }
+    }
 
-  public void setPrice(BigDecimal price) {
+    public void setPrice(BigDecimal price) {
     this.price = price;
-  }
+    }
 
-  public String getImgUrl() {
+    public String getImgUrl() {
     return imgUrl;
-  }
+    }
 
-  public void setImgUrl(String imgUrl) {
+    public void setImgUrl(String imgUrl) {
     this.imgUrl = imgUrl;
-  }
+    }
 
-  public Category getCategory() {
+    public Category getCategory() {
     return category;
-  }
+    }
 
-  public void setCategory(Category category) {
+    public void setCategory(Category category) {
     this.category = category;
-  }
+    }
 
-  public String getName() {
+    public String getName() {
     return name;
-  }
+    }
 
-  public void setName(String name) {
+    public void setName(String name) {
     this.name = name;
-  }
+    }
 
-  public String getType() {
+    public String getType() {
     return type;
-  }
+    }
 
-  public void setType(String type) {
+    public void setType(String type) {
     this.type = type;
-  }
+    }
 
-  public String getDescription() {
+    public String getDescription() {
     return description;
-  }
+    }
 
-  public void setDescription(String description) {
+    public void setDescription(String description) {
     this.description = description;
-  }
+    }
 
-  public User getUser() {
+    public User getUser() {
     return user;
-  }
+    }
 
-  public void setUser(User user) {
+    public void setUser(User user) {
     this.user = user;
-  }
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        Product product = (Product) o;
+//
+//        return id.equals(product.id);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return id.hashCode();
+//    }
 }
--- src/main/java/com/example/springblog/repo/OrderStatusRepository.java
+++ src/main/java/com/example/springblog/repo/OrderStatusRepository.java
@@ -1,9 +1,10 @@
 package com.example.springblog.repo;
 
 import com.example.springblog.models.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderStatusRepository extends CrudRepository<OrderStatus, Long> {
+public interface OrderStatusRepository extends JpaRepository<OrderStatus, Long> {
     OrderStatus findByStatus(String status);
 
     OrderStatus findOrderStatusById(long id);
