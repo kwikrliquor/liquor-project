54c169960d86a6f965e800cd352b74f84ed6a7db
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-01-30 09:14:42 -0600

    changed to where new user gets age_verified set to null

--- src/main/java/com/example/springblog/controllers/UserController.java
+++ src/main/java/com/example/springblog/controllers/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
     }
 
     user.setPassword(passwordEncoder.encode(user.getPassword()));
-    user.setAgeVerified(false);
+//    user.setAgeVerified(false);
 
     // Custom validation if the username is taken
 
